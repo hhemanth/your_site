e45c842c1d0272fe0fa0b608c00559d7004b5431
user_1 <user_1@codelearn.net>
2013-01-14 11:31:40 +0000

    Default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YourSite::Application.config.secret_token = 'a479546eb04348449fffba7ef633f56b3ae94e5bea815738dedd334859454319d79eabe8e1a341eaabb9ef397c8db5c86e1b6362b2a3b44542c0b58ae6c0a53b'
